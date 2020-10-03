7d3a08fb2924740a79e8dbaaf3350d1c48d57c03
adamb70 <adam.boaler@hotmail.com>
2020-10-03 03:18:55 +0100

    Personalisation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,31 @@
 (() => {
-  // Theme switch
-  const body = document.body;
-  const lamp = document.getElementById("mode");
+	// Theme switch
+	const body = document.body;
+	const lamp = document.getElementById("mode");
 
-  const toggleTheme = (state) => {
-    if (state === "dark") {
-      localStorage.setItem("theme", "light");
-      body.removeAttribute("data-theme");
-    } else if (state === "light") {
-      localStorage.setItem("theme", "dark");
-      body.setAttribute("data-theme", "dark");
-    } else {
-      initTheme(state);
-    }
-  };
+	const toggleTheme = (state) => {
+		if (state === "dark") {
+			localStorage.setItem("theme", "light");
+			body.removeAttribute("data-theme");
+		} else if (state === "light") {
+			localStorage.setItem("theme", "dark");
+			body.setAttribute("data-theme", "dark");
+		} else {
+			initTheme(state);
+		}
+	};
 
-  lamp.addEventListener("click", () =>
-    toggleTheme(localStorage.getItem("theme"))
-  );
+	lamp.addEventListener("click", () =>
+		toggleTheme(localStorage.getItem("theme"))
+	);
 
-  // Blur the content when the menu is open
-  const cbox = document.getElementById("menu-trigger");
+	// Blur the content when the menu is open
+	const cbox = document.getElementById("menu-trigger");
 
-  cbox.addEventListener("change", function () {
-    const area = document.querySelector(".wrapper");
-    this.checked
-      ? area.classList.add("blurry")
-      : area.classList.remove("blurry");
-  });
+	cbox.addEventListener("change", function () {
+		const area = document.querySelector(".wrapper");
+		this.checked
+			? area.classList.add("blurry")
+			: area.classList.remove("blurry");
+	});
 })();
